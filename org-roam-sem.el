802d809be29e7a42f2d2afc590cca2d7947fcf09
Abhinav Tushar <abhinav@lepisma.xyz>
2025-01-20 09:10:38 +0530

    Remove separate python server

--- org-roam-sem.el
+++ org-roam-sem.el
@@ -26,9 +26,6 @@
 
 ;;; Code:
 
-
-(require 'url)
-(require 'json)
 (require 'cl-generic)
 (require 'org-roam)
 (require 'org-roam-category)
@@ -49,18 +46,6 @@
 
 (defvar org-roam-sem-db nil
   "Placeholder for sem db.")
-
-(defun org-roam-sem--request (path)
-  "Send GET request to sem server with given PATH and return json
-response."
-  (let* ((url (format "http://localhost:8431%s" path))
-         (buffer (url-retrieve-synchronously url)))
-    (when buffer
-      (with-current-buffer buffer
-        (goto-char (point-min))
-        (re-search-forward "\n\n")
-        (let ((json-array-type 'list))
-          (json-read))))))
 
 (defun org-roam-sem-setup ()
   "Connect to database, creating it and needed tables if not already
@@ -96,25 +81,59 @@ the 'content' that's used for generating embedding."
 
 (defun org-roam-sem-store-nodes (nodes)
   "Store all NODES in the database."
-  (let ((batch-size 50)
-        batch)
-    (mapc (lambda (node)
-            (push node batch)
-            (when (= batch-size (length batch))
-              (sem-add-batch org-roam-sem-db batch #'org-roam-node-embed-batch
-                             #'org-roam-node--sem-write-fn org-roam-sem-nodes-table t)
-              (setq batch nil)))
-          nodes)
-    (when batch
-      (sem-add-batch org-roam-sem-db batch #'org-roam-node-embed-batch
-                     #'org-roam-node--sem-write-fn org-roam-sem-nodes-table t))))
+  (sem-add-batch-chunked org-roam-sem-db nodes #'org-roam-node-embed-batch 50
+                         #'org-roam-node--sem-write-fn org-roam-sem-nodes-table t))
 
+(defun org-roam-sem--link-context (link-data)
+  "Get surrounding text context for the given LINK-DATA."
+  (let ((source-node (org-roam-populate (org-roam-node-create :id (nth 0 link-data)))))
+    (save-excursion
+      (with-current-buffer (find-file-noselect (org-roam-node-file source-node))
+        (goto-char (nth 2 link-data))
+        (buffer-substring-no-properties (line-beginning-position) (line-end-position))))))
+
+(defun org-roam-sem--link-categories (link-data)
+  "Return a list of categories that specify the link in LINK-DATA.
+
+TODO: This could be designed better.  The main purpose is to tell what
+type of link one is."
+  (let ((source-node (org-roam-populate (org-roam-node-create :id (nth 0 link-data))))
+        (dest-node (org-roam-populate (org-roam-node-create :id (nth 1 link-data)))))
+    (list (org-roam-node-category source-node)
+          (org-roam-node-category dest-node))))
+
+(defun org-roam-links ()
+  "Return a list of all id type links stored in the org-roam database.
+
+This might be a heavy operation since reading link context involves
+opening buffers where link is established."
+  (let ((sql [:select [source dest pos properties] :from links :where (= type "id")]))
+    (mapcar (lambda (link-data)
+              (list :source-id (nth 0 link-data)
+                    :dest-id (nth 1 link-data)
+                    :context (org-roam-sem--link-context link-data)
+                    :categories (org-roam-sem--link-categories link-data)))
+            (org-roam-db-query sql))))
+
+(defun org-roam-sem-link-embed-batch (links)
+  "Embed a list of LINKS."
+  (sem-embed-default links (lambda (link) (plist-get link :context))))
+
+(defun org-roam-sem-store-links (links)
+  "Save LINKS for sem search."
+  (sem-add-batch-chunked org-roam-sem-db links #'org-roam-sem-link-embed-batch 50
+                         #'prin1-to-string org-roam-sem-links-table t))
+
+;;;###autoload
 (defun org-roam-sem-sync ()
   "Sync all nodes and links with the vector storage."
   (interactive)
-  (let ((nodes (org-roam-node-list)))
+  (let ((nodes (org-roam-node-list))
+        (links (org-roam-links)))
     (org-roam-sem-store-nodes nodes)
-    (message "Upserted %d nodes" (length nodes))))
+    (message "Upserted %d nodes" (length nodes))
+    (org-roam-sem-store-links links)
+    (message "Stored %d links" (length links))))
 
 (defun org-roam-sem-similar (node)
   "Find nodes similar to NODE by matching with stored vectors.
@@ -126,55 +145,30 @@ TODO: Also add score filtering at 0.6."
                              (car it)))
           (sem-similar org-roam-sem-db node 10 #'org-roam-node-embed #'read org-roam-sem-nodes-table)))
 
-(defun org-roam-sem--request-search-links (term)
-  (org-roam-sem--request (format "/search/links/%s" (url-encode-url term))))
-
-(defun org-roam-sem--link-context (link-data)
-  (let ((source-node (org-roam-populate (org-roam-node-create :id (nth 0 link-data)))))
-    (save-excursion
-      (with-current-buffer (find-file-noselect (org-roam-node-file source-node))
-        (goto-char (nth 2 link-data))
-        (buffer-substring-no-properties (line-beginning-position) (line-end-position))))))
-
-(defun org-roam-sem--link-categories (link-data)
-  (let ((source-node (org-roam-populate (org-roam-node-create :id (nth 0 link-data))))
-        (dest-node (org-roam-populate (org-roam-node-create :id (nth 1 link-data)))))
-    (list (org-roam-node-category source-node)
-          (org-roam-node-category dest-node))))
-
-(defun org-roam-sem-prep-links (output-filepath)
-  "Collect links and save metadata for OUTPUT-FILEPATH for sem search."
-  (let* ((sql [:select [source dest pos properties] :from links :where (= type "id")])
-         (output (mapcar (lambda (link-data)
-                           (list :source-id (nth 0 link-data)
-                                 :dest-id (nth 1 link-data)
-                                 :context (org-roam-sem--link-context link-data)
-                                 :categories (org-roam-sem--link-categories link-data)))
-                         (org-roam-db-query sql))))
-    (with-temp-file output-filepath
-      (insert (json-encode (list :links (vconcat output)))))))
-
 (defun org-roam-sem--insert-link-results (results)
-  "Insert the search RESULTS from semantic link search."
+  "Insert the search RESULTS from semantic link search.
+
+Each item in results is a pair of score and link."
   ;; We group the results in link types (category to category mapping)
-  (let ((groups (seq-group-by (lambda (result) (alist-get 'categories (car result))) results)))
-    (dolist (group (seq-sort-by (lambda (group-pair) (cadar (cdr group-pair))) #'> groups))
+  (let ((groups (seq-group-by (lambda (result) (plist-get (cdr result) :categories)) results)))
+    (dolist (group (seq-sort-by (lambda (group-pair) (caar (cdr group-pair))) #'> groups))
       (magit-insert-section (org-roam-link-category-section)
         (magit-insert-heading (format "%s (%s)" (string-join (car group) " ↔ ") (length (cdr group))))
         (dolist (result (cdr group))
-          (let ((source-node (org-roam-populate (org-roam-node-create :id (alist-get 'source-id (car result))))))
+          (let ((source-node (org-roam-populate (org-roam-node-create :id (plist-get (cdr result) :source-id)))))
             (magit-insert-section (org-roam-link-section)
-              (magit-insert-heading (concat (format "[%.3f] " (cadr result))
+              (magit-insert-heading (concat (format "[%.3f] " (car result))
                                             (propertize (org-roam-node-title source-node)
                                                         'font-lock-face 'org-roam-title)))
               (magit-insert-section section (org-roam-preview-section)
                                     (oset section file (org-roam-node-file source-node))
-                                    (insert (org-roam-fontify-like-in-org-mode (alist-get 'context (car result))) "\n\n")))))))))
+                                    (insert (org-roam-fontify-like-in-org-mode (plist-get (cdr result) :context)) "\n\n")))))))))
 
 (defun org-roam-sem-search-links (term)
   "Search links using semantic search using the TERM."
   (interactive "sSearch: ")
-  (if-let ((results (org-roam-sem--request-search-links term)))
+  (if-let ((results (sem-similar org-roam-sem-db term 10 (lambda (text) (aref (sem-embed-default (list text)) 0))
+                                 #'read org-roam-sem-links-table)))
       (progn
         (with-current-buffer (get-buffer-create "*org-roam-link-search*")
           (org-roam-mode)
